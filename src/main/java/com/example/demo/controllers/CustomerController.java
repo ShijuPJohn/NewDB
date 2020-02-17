5144272cce479242b873cd5138a195e29611b612
ShijuPJohn <shijupjohn4u@gmail.com>
2020-02-17 13:05:13 +0530

    add a view and get method for the view

--- src/main/java/com/example/demo/controllers/CustomerController.java
+++ src/main/java/com/example/demo/controllers/CustomerController.java
@@ -2,25 +2,39 @@ package com.example.demo.controllers;
 
 import com.example.demo.Customer;
 import com.example.demo.dao.CustomerDAO;
+import com.sun.org.apache.xpath.internal.operations.Mod;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
-@RestController
+@Controller
 public class CustomerController {
     @Autowired
     public CustomerDAO dao;
 
 
     @GetMapping("/users/{id}")
-    public Customer get(@PathVariable("id") int id) {
-        return dao.select(id);
+    public String get(@PathVariable("id") int id, Model model) {
+        Customer customer = dao.select(id);
+        model.addAttribute("id",customer.getId());
+        model.addAttribute("first_name",customer.getFirstName());
+        model.addAttribute("last_name",customer.getLastName());
+        return "getid";
+    }
+
+
+    @GetMapping("/users/print")
+    public String get(@RequestParam(defaultValue = "shiju",required = false)  String name, Model model) {
+        model.addAttribute("user",name);
+        return "hello";
     }
 
 
     @PostMapping(path = "/users")
     public String post(@RequestBody Customer newCustomer) {
         dao.insert(newCustomer);
-        return "Success";
+        return "hello";
     }
 
     @DeleteMapping(path = "/users/{id}")
