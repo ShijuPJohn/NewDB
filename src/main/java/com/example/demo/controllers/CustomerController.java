6c05c7e6f67adf142682b384a72b0670f927b93c
ShijuPJohn <shijupjohn4u@gmail.com>
2020-02-25 18:19:57 +0530

    add an option in index page login with facebook

--- src/main/java/com/example/demo/controllers/CustomerController.java
+++ src/main/java/com/example/demo/controllers/CustomerController.java
@@ -3,11 +3,19 @@ package com.example.demo.controllers;
 import com.example.demo.Customer;
 import com.example.demo.LoginObject;
 import com.example.demo.dao.CustomerDAO;
+import org.apache.catalina.User;
 import org.mindrot.jbcrypt.BCrypt;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.social.connect.Connection;
+import org.springframework.social.facebook.api.Facebook;
+import org.springframework.social.facebook.connect.FacebookConnectionFactory;
+import org.springframework.social.oauth2.AccessGrant;
+import org.springframework.social.oauth2.OAuth2Operations;
+import org.springframework.social.oauth2.OAuth2Parameters;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
@@ -17,6 +25,42 @@ import java.util.List;
 public class CustomerController {
     @Autowired
     public CustomerDAO dao;
+
+
+    private FacebookConnectionFactory factory = new FacebookConnectionFactory("233585174315729",
+            "c2d3b1e92e9b052ea6119d4c79758bac");
+
+    @GetMapping(value = "/useApplication")
+    public String producer() {
+
+        OAuth2Operations operations = factory.getOAuthOperations();
+        OAuth2Parameters params = new OAuth2Parameters();
+
+        params.setRedirectUri("/forwardLogin");
+        params.setScope("email,public_profile");
+
+        String url = operations.buildAuthenticateUrl(params);
+        System.out.println("The URL is" + url);
+        return "redirect:" + url;
+
+    }
+
+    @RequestMapping(value = "/forwardLogin")
+    public ModelAndView prodducer(@RequestParam("code") String authorizationCode) {
+        OAuth2Operations operations = factory.getOAuthOperations();
+        AccessGrant accessToken = operations.exchangeForAccess(authorizationCode, "/forwardLogin",
+                null);
+
+        Connection<Facebook> connection = factory.createConnection(accessToken);
+        Facebook facebook = connection.getApi();
+        String[] fields = {"id", "email", "first_name", "last_name"};
+        User userProfile = facebook.fetchObject("me", User.class, fields);
+        ModelAndView model = new ModelAndView("details");
+        model.addObject("user", userProfile);
+        return model;
+
+    }
+
 
     @GetMapping("/")
     public String indexGet(Model model) {
@@ -51,12 +95,12 @@ public class CustomerController {
         Customer loggedInCustomer = (Customer) session.getAttribute("user");
         if (loggedInCustomer.getIsAdmin()) {
             List<Customer> customer = dao.selectAll();
-            model.addAttribute("listOfObjects",customer);
+            model.addAttribute("listOfObjects", customer);
             model.addAttribute("isAdmin", "Set");
         }
         model.addAttribute("loggedInCustomer", loggedInCustomer);
         System.out.println(loggedInCustomer.getIsAdmin());
-        model.addAttribute("isAdmin",loggedInCustomer.getIsAdmin());
+        model.addAttribute("isAdmin", loggedInCustomer.getIsAdmin());
         return "home";
     }
 
@@ -66,11 +110,12 @@ public class CustomerController {
         session.invalidate();
         return "logout";
     }
+
     @GetMapping("/editadmin/{id}")
     public String editAdmin(@PathVariable int id, Model model, HttpServletRequest request) {
         HttpSession session = request.getSession();
         Customer loggedInCustomer = (Customer) session.getAttribute("user");
-        if (loggedInCustomer!=null){
+        if (loggedInCustomer != null) {
             Customer customerToEdit = dao.select(id);
             model.addAttribute("CustomerToBeEdited", customerToEdit);
         }
@@ -100,11 +145,10 @@ public class CustomerController {
     }
 
 
-
     @GetMapping("/users/delete/{id}")
     public String deleteG(@PathVariable int id, HttpServletRequest request) {
         HttpSession session = request.getSession();
-        if (session!=null) {
+        if (session != null) {
             dao.delete(id);
         }
         return "success";
@@ -128,7 +172,7 @@ public class CustomerController {
 
 
     @PostMapping(path = "/users")
-    public String post(@ModelAttribute Customer newCustomer,HttpSession session) {
+    public String post(@ModelAttribute Customer newCustomer, HttpSession session) {
         String hashedPw = BCrypt.hashpw(newCustomer.getPassword(), BCrypt.gensalt());
         newCustomer.setPassword(hashedPw);
         dao.insert(newCustomer);
